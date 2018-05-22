20174396715f75d352a24b0860017dcbdefe2608
David Arcoleo <darcoleo@splunk.com>
2018-05-22 13:08:12 -0700

    update

--- bin/generate.py
+++ bin/generate.py
@@ -7,10 +7,12 @@ from pprint import pprint
 current_path = os.path.dirname(os.path.abspath(__file__))
 # print(current_path)
 
-f = []
-for (dirpath, dirnames, filenames) in os.walk(os.path.join(current_path, 'books')):
-    f.extend(filenames)
+file_list = []
+for (dirpath, dirnames, filenames) in os.walk(os.path.join(current_path, '..', 'books')):
+    file_list.extend(filenames)
     break
+file_list = sorted(file_list)
+# pprint(file_list)
 
 # print(f)
 
@@ -31,12 +33,19 @@ def generate_output(books):
         curr_subjects = book.get('subjects')
         for subject in curr_subjects:
             if subject in outline.keys():
-                outline[subject]['books'].append(
-                    {'title': book.get('title'), 'filename': book.get('filename')})
+                outline[subject]['books'].append({
+                    'title': book.get('title'), 
+                    'filename': book.get('filename'),
+                    'authors': ';'.join(book.get('authors'))
+                })
             else:
                 outline[subject] = {
                     'level': 2, 
-                    'books': [{'title': book.get('title'), 'filename': book.get('filename')}]}
+                    'books': [{
+                        'title': book.get('title'), 
+                        'filename': book.get('filename'),
+                        'authors': ';'.join(book.get('authors'))
+                    }]}
 
     for topic, value in sorted(outline.items()):
         # print(topic)
@@ -48,7 +57,7 @@ def generate_output(books):
         for book in value['books']:
             # print('----! %s' % str(book))
             output.append('')
-            output.append('link:books/%(filename)s[%(title)s]' % book)
+            output.append('link:books/%(filename)s[%(title)s, %(authors)s]' % book)
         
 
 
@@ -58,21 +67,27 @@ def generate_output(books):
     for line in output:
         print(line)
 
-for curr_file in f:
-    open_file = os.path.join(current_path, 'books', curr_file)
+for curr_file in file_list:
+    open_file = os.path.join(current_path, '..', 'books', curr_file)
     with open(open_file) as f:
         lines = f.readlines()
         current_title = None
         dsa_dict = None
-        for line in lines:
+        author_line = None
+        for idx, line in enumerate(lines):
             if line.startswith('==='):
                 current_title = line[len('==='):].strip()
+                author_line = idx + 1
+            if idx == author_line:
+                current_authors = line.split(';')
+                current_authors = list(map(str.strip, current_authors))
             if line.startswith(':dsa:'):
                 # print('Hello -- %s' % line)
                 current_line = line[len(':dsa:'):].strip()
                 dsa_dict = literal_eval(current_line)
                 dsa_dict['title'] = current_title
                 dsa_dict['filename'] = curr_file
+                dsa_dict['authors'] = current_authors
                 books_list.append(dsa_dict)
                 # print(dsa_dict)
 
