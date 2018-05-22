99fcb7832e705b395d740c1c4052b925d4aa8f83
David Arcoleo <darcoleo@splunk.com>
2018-05-22 16:21:38 -0700

    updatE

--- bin/generate.py
+++ bin/generate.py
@@ -11,11 +11,16 @@ from pprint import pprint
 books_list = []
 # parse_tags(tag_dict):
 
-def generate_output(books):
+def generate_output(args, books):
     output = ['= Welcome to the Books Wiki']
     output.append('  David Arcoleo <david@arcoleo.org>')
 
     outline = {}
+
+    if args.table:
+        divider = ' | '
+    else:
+        divider = ', '
 
     for book in books:
         # Get subjects
@@ -43,14 +48,20 @@ def generate_output(books):
         output.append('')
         level = '=' * value['level']
         output.append('%s %s' % (level, topic))
-        output.append('|====================')
+        if args.table:
+            output.append('|====================')
         # print('--! %s' % value)
         for book in value['books']:
             # print('----! %s' % str(book))
-            output.append('')
-            output.append('link:books/%(filename)s[%(title)s] | %(authors)s' % book)
+            if not args.table:
+                output.append('')
+            title = 'link:books/%(filename)s[%(title)s]' % book
+            authors = '%(authors)s' % book
+            line = '%s%s%s' % (title, divider, authors)
+            output.append(line)
 
-    output.append('|====================')    
+    if args.table:
+        output.append('|====================')    
 
 
     # pprint(outline)
@@ -61,9 +72,15 @@ def generate_output(books):
         print(line)
     
 
+def read_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--table', action='store_true')
+    args = parser.parse_args()
 
+    return args
 
 def main():
+    args = read_args()
     file_list = []
 
     current_path = os.path.dirname(os.path.abspath(__file__))
@@ -111,7 +128,7 @@ def main():
     # pprint(books_list)
 
     # print('--- Generated ---')
-    generate_output(books_list)
+    generate_output(args, books_list)
 
 if __name__ == '__main__':
     main()
